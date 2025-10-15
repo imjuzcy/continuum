3ec6642d0ec3be0fef3335162716f2cd29bc7b09
imjuzcy <12483281+imjuzcy@users.noreply.github.com>
2025-10-15 18:15:12 +0200

    don't switch to compact layout

--- app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
@@ -434,10 +434,6 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
             if (post != null) {
                 switch (post.getPostType()) {
                     case Post.VIDEO_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         if (mAutoplay) {
                             if ((!mAutoplayNsfwVideos && post.isNSFW()) || post.isSpoiler()) {
                                 return VIEW_TYPE_POST_CARD_WITH_PREVIEW_TYPE;
@@ -448,23 +444,11 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
                         return VIEW_TYPE_POST_CARD_WITH_PREVIEW_TYPE;
                     case Post.GIF_TYPE:
                     case Post.IMAGE_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_WITH_PREVIEW_TYPE;
                     case Post.GALLERY_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_GALLERY_TYPE;
                     case Post.LINK_TYPE:
                     case Post.NO_PREVIEW_LINK_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         switch (mDefaultLinkPostLayout) {
                             case SharedPreferencesUtils.POST_LAYOUT_CARD_2:
                                 return VIEW_TYPE_POST_CARD_2_WITH_PREVIEW_TYPE;
@@ -535,10 +519,6 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
             if (post != null) {
                 switch (post.getPostType()) {
                     case Post.VIDEO_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         if (mAutoplay) {
                             if ((!mAutoplayNsfwVideos && post.isNSFW()) || post.isSpoiler()) {
                                 return VIEW_TYPE_POST_CARD_2_WITH_PREVIEW_TYPE;
@@ -549,23 +529,11 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
                         return VIEW_TYPE_POST_CARD_2_WITH_PREVIEW_TYPE;
                     case Post.GIF_TYPE:
                     case Post.IMAGE_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_2_WITH_PREVIEW_TYPE;
                     case Post.GALLERY_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_2_GALLERY_TYPE;
                     case Post.LINK_TYPE:
                     case Post.NO_PREVIEW_LINK_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         switch (mDefaultLinkPostLayout) {
                             case SharedPreferencesUtils.POST_LAYOUT_CARD:
                                 return VIEW_TYPE_POST_CARD_WITH_PREVIEW_TYPE;
@@ -589,10 +557,6 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
             if (post != null) {
                 switch (post.getPostType()) {
                     case Post.VIDEO_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         if (mAutoplay) {
                             if ((!mAutoplayNsfwVideos && post.isNSFW()) || post.isSpoiler()) {
                                 return VIEW_TYPE_POST_CARD_3_WITH_PREVIEW_TYPE;
@@ -603,23 +567,11 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
                         return VIEW_TYPE_POST_CARD_3_WITH_PREVIEW_TYPE;
                     case Post.GIF_TYPE:
                     case Post.IMAGE_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_3_WITH_PREVIEW_TYPE;
                     case Post.GALLERY_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         return VIEW_TYPE_POST_CARD_3_GALLERY_TYPE;
                     case Post.LINK_TYPE:
                     case Post.NO_PREVIEW_LINK_TYPE:
-                        if (shouldUseCompactLayout(post)) {
-                            return VIEW_TYPE_POST_COMPACT;
-                        }
-
                         switch (mDefaultLinkPostLayout) {
                             case SharedPreferencesUtils.POST_LAYOUT_CARD:
                                 return VIEW_TYPE_POST_CARD_WITH_PREVIEW_TYPE;
@@ -1483,14 +1435,6 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
             return new Post.Preview(thumbnailUrl, 0, 0, "", "");
         }
         return preview;
-    }
-
-    private boolean hasValidThumbnailFallback(String thumbnailUrl) {
-        return thumbnailUrl != null && !thumbnailUrl.isEmpty() && !thumbnailUrl.equals("self") && !thumbnailUrl.equals("default") && !thumbnailUrl.equals("nsfw") && !thumbnailUrl.equals("spoiler") && !thumbnailUrl.equals("image") && thumbnailUrl.startsWith("http");
-    }
-
-    private boolean shouldUseCompactLayout(Post post) {
-        return (post.getPreviews() == null || post.getPreviews().isEmpty()) && !hasValidThumbnailFallback(post.getThumbnailUrl());
     }
 
     private void loadImage(final RecyclerView.ViewHolder holder) {
