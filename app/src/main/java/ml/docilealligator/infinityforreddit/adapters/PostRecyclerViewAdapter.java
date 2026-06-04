eb88c02e9730c9c7480caffa1337603bc4922007
imjuzcy <12483281+imjuzcy@users.noreply.github.com>
2026-06-04 18:52:10 +0000

    don't switch to compact layout

--- app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/adapters/PostRecyclerViewAdapter.java
@@ -1584,7 +1584,7 @@ public class PostRecyclerViewAdapter extends PagingDataAdapter<Post, RecyclerVie
     }
 
     private boolean shouldUseCompactLayout(Post post) {
-        return (post.getPreviews() == null || post.getPreviews().isEmpty()) && !hasValidThumbnailFallback(post.getThumbnailUrl());
+        return false;
     }
 
     private int getTypeColor(int postType) {
